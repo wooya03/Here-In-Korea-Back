6fd9ff834813e794d446e566ff96bbc4d7e216cd
Wooya <wooya_0@naver.com>
2024-07-22 11:34:30 +0900

    first commit

---
+++ src/main/java/kr/kro/hereinkorea/HereinkoreaApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.hereinkorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereinkoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereinkoreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/kro/hereinkorea/HereinkoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.hereinkorea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereinkoreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
